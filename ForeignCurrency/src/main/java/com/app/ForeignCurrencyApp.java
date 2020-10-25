e2cdfba4911a6ce65842517ab0df598f487ce758
NarasimhaRaoNiki <73018939+NarasimhaRaoNiki@users.noreply.github.com>
2020-10-25 15:17:46 +0530

    Update ForeignCurrencyApp.java

--- ForeignCurrency/src/main/java/com/app/ForeignCurrencyApp.java
+++ ForeignCurrency/src/main/java/com/app/ForeignCurrencyApp.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
+/**
+	 * spring main method 
+	 */
 @SpringBootApplication
 public class ForeignCurrencyApp {
 	public static void main(String[] args) 
